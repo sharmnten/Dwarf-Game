adc80261d73ed4eaacfb72ecb35daa79fd532583
Sharmnzero <nsharma26@eagleschool.org>
2024-07-13 17:26:09 +0000

    added sprite, setted up animation

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -1,13 +1,50 @@
 import kaplay from "kaplay";
-import "kaplay/global";
+kaplay()
 
-const k = kaplay()
+loadSpriteAtlas("sprites/Dworf.png", {
+    "dworf": {
+        x: 0,
+        y: 0,
+        width: 64,
+        height: 72,
+        sliceX: 4,
+		sliceY: 3,
+        anims: {
+            noAnim: 0,
+            idle: { from: 4, to: 7 },
+            run:{from: 8, to:11},
+        },
+    },
+})
 
-k.loadSprite("bean", "sprites/bean.png")
-
-k.add([
-	k.pos(120, 80),
-	k.sprite("bean"),
+const dworf=add([
+  sprite("dworf"),
+  pos(400,400),
+  scale(5,5),
 ])
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+
+onKeyPress("right",()=>{
+    dworf.play("run",{loop:true});
+    dworf.flipX = false;
+})
+
+onKeyDown("right",()=>{
+    dworf.move(600,0);
+})
+onKeyRelease("right",()=>{
+    dworf.play("idle",{loop:true});
+})
+
+
+onKeyPress("left",()=>{
+    dworf.play("run",{loop:true});
+    dworf.flipX = true;
+})
+
+onKeyDown("left",()=>{
+    dworf.move(-600,0);
+})
+onKeyRelease("left",()=>{
+    dworf.play("idle",{loop:true});
+})
